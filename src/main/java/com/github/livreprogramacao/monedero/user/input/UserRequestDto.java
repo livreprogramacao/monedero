483f7919b98ee0274cf5e13b2f02d7cc3a8ab55a
Fabio Santos Almeida <livre.programacao@gmail.com>
2026-01-03 04:58:26 -0300

    DTOs.
    For incoming requests.

--- src/main/java/com/github/livreprogramacao/monedero/user/input/UserRequestDto.java
+++ src/main/java/com/github/livreprogramacao/monedero/user/input/UserRequestDto.java
@@ -1,4 +1,12 @@
 package com.github.livreprogramacao.monedero.user.input;
 
+@lombok.extern.slf4j.Slf4j
 public class UserRequestDto {
+
+    @lombok.NonNull
+    private String username;
+
+    @javax.validation.constraints.Email
+    private String email;
+
 }
