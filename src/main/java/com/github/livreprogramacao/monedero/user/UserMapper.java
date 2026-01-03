d3a73ea81c1ae8f0b511514ac720a42c7453430e
Fabio Santos Almeida <livre.programacao@gmail.com>
2026-01-03 05:05:24 -0300

    Mapper.
    Mapper declaration.

--- src/main/java/com/github/livreprogramacao/monedero/user/UserMapper.java
+++ src/main/java/com/github/livreprogramacao/monedero/user/UserMapper.java
@@ -1,4 +1,10 @@
 package com.github.livreprogramacao.monedero.user;
 
-public class UserMapper {
+@org.mapstruct.Mapper
+public interface UserMapper {
+
+    com.github.livreprogramacao.monedero.user.output.User toEntity(com.github.livreprogramacao.monedero.user.input.UserRequestDto dto);
+
+    com.github.livreprogramacao.monedero.user.input.UserResponseDto toDto(com.github.livreprogramacao.monedero.user.output.User user);
+
 }
