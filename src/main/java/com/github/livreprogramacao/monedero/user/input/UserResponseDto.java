82b448ea41ca415b9a3e1a7a0844a748c218af1a
Fabio Santos Almeida <livre.programacao@gmail.com>
2026-01-03 04:59:05 -0300

    DTOs.
    For outgoing responses.

--- src/main/java/com/github/livreprogramacao/monedero/user/input/UserResponseDto.java
+++ src/main/java/com/github/livreprogramacao/monedero/user/input/UserResponseDto.java
@@ -1,4 +1,20 @@
 package com.github.livreprogramacao.monedero.user.input;
 
 public class UserResponseDto {
+
+    @lombok.NonNull
+    private Long Id;
+
+    @lombok.NonNull
+    private String username;
+
+    @lombok.NonNull
+    private String email;
+
+    @lombok.NonNull
+    private String status;
+
+    @lombok.NonNull
+    private String role;
+
 }
