22f4508426036c6a7c29572f5205073cdde197e6
Fabio Santos Almeida <livre.programacao@gmail.com>
2026-01-03 05:54:00 -0300

    Unit test.

--- src/test/java/com/github/livreprogramacao/monedero/AppTest.java
+++ src/test/java/com/github/livreprogramacao/monedero/AppTest.java
@@ -1,38 +1,13 @@
 package com.github.livreprogramacao.monedero;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+class AppTest {
+
+    @Test
+    void additionTest() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
